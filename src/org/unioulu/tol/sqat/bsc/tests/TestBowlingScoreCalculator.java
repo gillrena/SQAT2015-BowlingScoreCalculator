b6ec02ee2fd0d7593b01734e7b8b769052b1de0f
gillrena <leagillrena@hotmail.com>
2015-10-14 16:44:51 +0300

    project finished

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestBowlingScoreCalculator {
-	private BowlingGame() game;
+	//BowlingGame game = new BowlingGame();
 
 	@Test
 	public void test() {
